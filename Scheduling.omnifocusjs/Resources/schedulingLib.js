c7da6d961a73f78bd8adfbd72f1d25dc252d398b
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-13 16:23:36 +1100

    refactor: :recycle: split date formatting into separate function

--- Scheduling.omnifocusjs/Resources/schedulingLib.js
+++ Scheduling.omnifocusjs/Resources/schedulingLib.js
@@ -2,14 +2,20 @@
 (() => {
   const schedulingLib = new PlugIn.Library(new Version('1.0'))
 
+  schedulingLib.getDateFormatter = () => {
+    return Formatter.Date.withStyle(Formatter.Date.Style.Medium)
+  }
+
   schedulingLib.getDateString = (date) => {
-    const formatter = Formatter.Date.withStyle(Formatter.Date.Style.Medium)
+    const formatter = schedulingLib.getDateFormatter()
     return formatter.stringFromDate(date)
   }
 
   schedulingLib.getString = (date) => {
     const dateString = schedulingLib.getDateString(date)
     if (Calendar.current.dateComponentsBetweenDates(new Date(), date) > 8) return dateString
+
+    // TODO: Use 'Tomorrow' for tomorrow
 
     // otherwise, date is in next 7 days - include day of week
     const dayFormatter = Formatter.Date.withFormat('EEEE')
@@ -23,13 +29,12 @@
   }
 
   schedulingLib.getTag = (date) => {
-    const name = schedulingLib.getDateString(date)
+    const dateString = schedulingLib.getDateString(date)
     const parent = schedulingLib.getSchedulingTag()
-    const tag = parent.children.find(tag => tag.name.includes(name))
-    if (tag !== null) return tag
+    const tag = parent.children.find(tag => tag.name.includes(dateString)) || new Tag(schedulingLib.getString(date), parent)
+    return tag
 
-    // else tag does not exist
-    return new Tag(name, parent)
+    // if tag does not exist
     // TODO: deal with existing tags that are not in this format?
     // TODO: sort tags by date
   }
