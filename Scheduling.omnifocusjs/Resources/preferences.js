8786fbf17bbe4c93a8ada0a69d349cd45212d64d
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-20 13:42:44 +1100

    feat: :lipstick: change order of preferences in dialogue

--- Scheduling.omnifocusjs/Resources/preferences.js
+++ Scheduling.omnifocusjs/Resources/preferences.js
@@ -4,18 +4,22 @@
     const syncedPrefs = this.schedulingLib.loadSyncedPrefs()
 
     // get current preferences or set defaults if they don't yet exist
+    const schedulingTag = this.schedulingLib.schedulingTag()
     const todayTag = this.schedulingLib.todayTag()
     const flagToday = syncedPrefs.readBoolean('flagToday')
-    const schedulingTag = this.schedulingLib.schedulingTag()
 
     // create and show form
     const form = new Form()
     const tagNames = flattenedTags.map(t => t.name)
+
+    form.addField(new Form.Field.Option('schedulingTag', 'Scheduling tag', flattenedTags, tagNames, schedulingTag, 'Please select a tag'))
+
     const todayTagField = new Form.Field.Option('todayTag', '\'Today\' tag', flattenedTags, tagNames, todayTag, 'None')
     todayTagField.allowsNull = true
     form.addField(todayTagField)
-    form.addField(new Form.Field.Option('schedulingTag', 'Scheduling tag', flattenedTags, tagNames, schedulingTag, 'Please select a tag'))
+
     form.addField(new Form.Field.Checkbox('flagToday', 'Flag denotes \'Today\' tasks', flagToday))
+    
     await form.show('Preferences: Agendas', 'OK')
 
     // save preferences
