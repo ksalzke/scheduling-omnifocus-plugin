147a73296b663d4cc840a02d07c149a3f09847ea
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2023-02-01 00:18:51 +1100

    feat: :sparkles: enable plug-in to act on projects, not just tasks
    closes #9

--- Scheduling.omnifocusjs/Resources/rescheduleTask.js
+++ Scheduling.omnifocusjs/Resources/rescheduleTask.js
@@ -1,11 +1,12 @@
 /* global PlugIn Form */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-    this.schedulingLib.promptAndReschedule(selection.tasks)
+
+    this.schedulingLib.promptAndReschedule([...selection.tasks, ...selection.projects.map(p => p.task)])
   })
 
   action.validate = function (selection, sender) {
-    return selection.tasks.length > 0
+    return [...selection.tasks, ...selection.projects].length > 0
   }
 
   return action
