c8fb05dba5ac2188583baf5bd570f80ed7ae26d7
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2023-06-17 22:06:19 +1000

    feat: :sparkles: allow unscheduling of projects, not just tasks

--- Scheduling.omnifocusjs/Resources/unscheduleTask.js
+++ Scheduling.omnifocusjs/Resources/unscheduleTask.js
@@ -1,11 +1,11 @@
 /* global PlugIn Form */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-    await this.schedulingLib.unscheduleTasks(selection.tasks)
+    await this.schedulingLib.unscheduleTasks([...selection.tasks, ...selection.projects.map(p => p.task)])
   })
 
   action.validate = function (selection, sender) {
-    return selection.tasks.length > 0
+    return [...selection.tasks, ...selection.projects].length > 0
   }
 
   return action
